fa4320893014bd0731eb5be12e7d5a4215ba35e7
Khandelwal <p.c.khandelwal@accenture.com>
2022-07-06 13:54:08 +0530

    Themes Extracted to another file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Catalog1'),
+        title: Text('Catalog'),
         centerTitle: true,
       ),
       body: Center(
